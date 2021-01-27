7eb128bb8e6116c31061b66d84039ed761a4caae
Hasan Eren Akgoz <202007012060@dogus.edu.tr>
2021-01-27 12:56:35 +0300

    Add project files.

---
+++ ClassMetotDemo/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+   public class Customer
+    {
+        public String MusteriTc { get; set; }
+        public String MusteriAdı { get; set; }
+        public String MusteriSoyad { get; set; }
+        public String MusteriMail { get; set; }
+        public String MusteriAdresi { get; set; }
+
+    }
+}
---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+   public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            customer.MusteriAdı = "Eren";
+            customer.MusteriSoyad = "Akgöz";
+            customer.MusteriMail = "HasanErenAkgz@gmail.com";
+            customer.MusteriTc = "111111111";
+            Console.WriteLine(customer.MusteriTc+" Eklendi");
+        }
+        public void Add(int Tc)
+        {
+            Console.WriteLine(Tc+" Numaralı müşteri silindi");
+        }
+    }
+}
